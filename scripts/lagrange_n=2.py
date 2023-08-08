23e43717331ccefd022a2f30d698d52d834ca9c6
marcusfschmidt <55887958+marcusfschmidt@users.noreply.github.com>
2023-08-08 23:53:37 +0200

    Added script for 2 DOF, animation for double pendulum

--- scripts/lagrange_n=2.py
+++ scripts/lagrange_n=2.py
@@ -1,169 +1,60 @@
 #%%
 import sympy as sp
 import numpy as np
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-#%%
-# Define the variables and symbols
-t = sp.symbols('t')
-x, y = sp.symbols('x y', cls=sp.Function)
+import matplotlib.animation as animation
 
-# Define the differential equations
-dx_dt = x(t).diff(t)
-dy_dt = y(t).diff(t)
-d2x_dt2 = x(t).diff(t, t)
-d2y_dt2 = y(t).diff(t, t)
+# Use style file for plots
+plt.style.use('../plotstyle.mplstyle')
+# Import plotting function to ensure consistency between plots
+import sys; sys.path.append(".."); from figSetup import figSetup
 
-eq1 = sp.Eq(d2x_dt2, -dy_dt)
-eq2 = sp.Eq(d2y_dt2, dx_dt)
+### Define constants; only some of them are used depending on the system
+g = 9.81 # Gravitational acceleration
+m = [1, 1] # Mass of particles
+l = [0.5, 0.8] # Length of pendulums
+k = [1, 1] # Spring constants
 
-# Initial conditions
-x0 = 1.0
-y0 = 0.0
-dx0_dt = 0
-dy0_dt = 1
+### Friction coefficients when including Rayleigh disspiation
+# Friction coefficient ~ v^0, e.g. friction from the normal force of a surface
+kineticFriction = np.array([0.1*m[0]*g, 0.1*m[1]*g])*0
+# Define the static friction coefficient when v ~ 0
+staticFriction = np.array([0.2*m[0]*g, 0.2*m[1]*g])*0
+# Flag for boolean flipping the sign of the friction coefficient when velocity changes sign
+flipSignBoolean = True
+# Friction coefficient ~ v^1
+linearFrictionCoefficient = np.array([0.1, 0.1])
+# If higher order friction is needed, the dissipation function must be changed accordingly
 
-#dsolve eq1 and eq2
-sol= sp.dsolve([eq1, eq2], ics={x(0): x0, y(0): y0, x(t).diff(t).subs(t, 0): dx0_dt, y(t).diff(t).subs(t, 0): dy0_dt})
-xSol, ySol = sol[0].rhs, sol[1].rhs
+### Initial conditions for the generalized coordinates and their deriatives 
+q0 = np.array([np.pi + 1e-10, np.pi+1e-10])
+v0 = np.array([0, 0])
+y0 = np.concatenate((q0, v0))
 
-#plot
-t_values = np.linspace(0, 10, 100)
-x_values = [xSol.subs(t, i) for i in t_values]
-y_values = [ySol.subs(t, i) for i in t_values]
+### Define the time array 
+ti = 0
+tf = 20
+t = np.linspace(ti, tf, 500)
 
-plt.plot(t_values, x_values, label='x(t)')
-plt.plot(t_values, y_values, label='y(t)')
-#%%
-
-# Convert to a system of first-order differential equations
-system = [dx_dt, dy_dt, eq1.rhs, eq2.rhs]
-
-# Function to convert the symbolic system to numerical functions
-func = sp.lambdify((x(t), y(t), dx_dt, dy_dt), system)
-
-def f_fromSympy(Y,t):
-    """Return the right-hand side of the differential equation
-    as a numpy array
-    """
-    return func(Y[0], Y[1], Y[2], Y[3])
-
-
-
-# Time array for integration
-t_values = np.linspace(0, 10, 100)
-
-# Solve the system using odeint
-initial_conditions = [x0, y0, dx0_dt, dy0_dt]
-sol = odeint(f_fromSympy, initial_conditions, t_values)
-
-# Extract the x and y values from the solution
-x_values, y_values = sol[:, 0], sol[:, 1]
-
-# Plot the results
-plt.plot(t_values, x_values, label='x(t)')
-plt.plot(t_values, y_values, label='y(t)')
-plt.xlabel('t')
-plt.ylabel('Values')
-plt.legend()
-plt.grid()
-plt.show()
-
-
-
-
-#%% POSSIBLE SOLUTION WITH 1 DOF
-###################################
-
+### Define step size for numerical differentiation
+h = 1e-4
+### Define the tolerances of the solver
+rtol = 1e-7
+atol = 1e-7
 
 
 import numpy as np
 import matplotlib.pyplot as plt
-h = 1e-6
-# Define the Lagrangian function for a simple pendulum
 
-def kinetic(q, q_dot, t, m = 1):
-    return 0.5*m*(q_dot**2)
-
-def potential(q, q_dot, t, m = 1, g = 9.81):
-        return m*g*(1 - np.cos(q))
-        # return m*g*q
-
-def lagrangian(q, q_dot, t):
-    m = 1  # Mass of the pendulum
-    g = 9.81  # Acceleration due to gravity
-    return (kinetic(q, q_dot, t) - potential(q, q_dot, t))*np.exp(0*t)
-
-def firstOrderDerivative(func, q, q_dot, t, h_q = 0, h_qdot = 0, h_t = 0):
-    return (func(q + h_q, q_dot + h_qdot, t + h_t) - func(q - h_q, q_dot - h_qdot, t - h_t)) / (2 * ( h_q + h_qdot + h_t))
-
-def secondOrderDerivative(func, q, q_dot, t, h_q = 0, h_qdot = 0, h_t = 0):
-    return (func(q + h_q, q_dot + h_qdot, t + h_t) - 2 * func(q, q_dot, t) + func(q - h_q, q_dot - h_qdot, t - h_t)) / (( h_q + h_qdot + h_t)**2)
-
-####
-def dL_dq(q, q_dot, t):
-    return firstOrderDerivative(lagrangian, q, q_dot, t, h_q = h)
-
-def dL_dqdot(q, q_dot, t):
-    return firstOrderDerivative(lagrangian, q, q_dot, t, h_qdot = h)
-
-###
-
-
-def ODE(y, t):
-    q, q_dot = y
-
-    dL_dqdot_dq = firstOrderDerivative(dL_dqdot, q, q_dot, t, h_q = h)
-    dL_dqdot_dqdot = secondOrderDerivative(lagrangian, q, q_dot, t, h_qdot = h)
-    dL_dqdot_dt = firstOrderDerivative(dL_dqdot, q, q_dot, t, h_t = h)
-    q_dot_dot = (dL_dq(q, q_dot, t) -  q_dot*dL_dqdot_dq - dL_dqdot_dt)/(dL_dqdot_dqdot)
-
-    return np.array([q_dot, q_dot_dot])
-
-# Initial conditions
-q0 = np.array([np.pi/2*0 + 0.1, 0])
-
-
-
-#solve with odeINT
-t = np.linspace(0, 3, 1000000)
-sol = odeint(ODE, q0, t)
-qt = sol[:, 0]
-q_dott = sol[:, 1]
-
-#plot the sol
-plt.figure()
-plt.plot(t, sol[:, 0], label='x')
-# plt.plot(t, sol[:, 1], label='Angular Velocity')
-
-analytical = q0[0] * np.cos(np.sqrt(9.81) * t)
-plt.plot(t, analytical, label='Analytical')
-
-#total energy
-totalEnergy = np.zeros_like(t)
-for i in range(len(t)):
-    totalEnergy[i] = kinetic(qt[i], q_dott[i], t[i]) + potential(qt[i], q_dott[i], t[i])
-
-plt.plot(t, totalEnergy, label='Total Energy')
-# %% IMPLEMENTING WITH MORE THAN 1 DOF
-######################################
-
-
-g = 9.81
-m1 = 1.0
-m2 = 1.0
-l1 = 1.0
-l2 = 1.0
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-h = 1e-6
-
-# Kinetic energy function
+### Define the kinetic and potential energy functions
 def kinetic(q, v, t):
     q1, q2 = q
     v1, v2 = v
+    m1, m2 = m
+    l1, l2 = l
+
+    # Kinetic energy terms for a double pendulum
     T1 = 0.5 * m1 * l1**2 * v1**2
     T2 = 0.5 * m2 * (l1**2 * v1**2 + l2**2 * v2**2 + 2 * l1 * l2 * v1 * v2 * np.cos(q1 - q2))
     return T1 + T2
@@ -171,56 +62,86 @@ def kinetic(q, v, t):
 # Potential energy function
 def potential(q, v, t):
     q1, q2 = q
+    m1, m2 = m
+    l1, l2 = l
+
+    # Potential energy terms for a double pendulum
     U1 = -m1 * g * l1 * np.cos(q1)
     U2 = -m2 * g * (l1 * np.cos(q1) + l2 * np.cos(q2))
     return U1 + U2
 
-
+### Define the Lagrangian with. j is a dummy variable needed to perform mixed partial derivatives.
+# Unlike for the n = 1 script, the damping function exp(ct) is not included and must be incorporated via the dissipation function.
 def lagrangian(q, q_dot, t, j = 0):
     return kinetic(q, q_dot, t) - potential(q, q_dot, t)
 
 
-
+### Define function to approximate the first order derivative of a function f(q, q_dot, t, j).
 def firstOrderDerivative(func, q, q_dot, t, i = 0, j = 0, H_q = 0, H_qdot = 0, H_t = 0):
+    """ 
+    func: function to be differentiated of the form f(q, q_dot, t, j)
+    q: values of q
+    q_dot: values of q_dot
+    t: value of t
+    i: index of q, q_dot or t to differentiate func with respect to
+    j: index of q, q_dot or t to feed to func as i, used for mixed partial derivatives
+    H_q: step size for q. Only one of H_q, H_qdot, H_t can be non-zero
+    H_qdot: step size for q_dot. Only one of H_q, H_qdot, H_t can be non-zero
+    H_t: step size for t. Only one of H_q, H_qdot, H_t can be non-zero
+    
+    """
+
+    # Define and populate arrays for step sizes to ensure differentation with respect to the correct variable
     h_q = np.zeros(2)
     h_qdot = np.zeros(2)
-
     h_q[i] = H_q
     h_qdot[i] = H_qdot
 
+    # Central difference approximation of the first derivative of O(h^2)
     return (func(q + h_q, q_dot + h_qdot, t + H_t, j = j) - func(q - h_q, q_dot - h_qdot, t - H_t, j = j)) / (2 * ( H_q + H_qdot + H_t))
 
-def secondOrderDerivative(func, q, q_dot, t, i = 0, j = 0, H_q = 0, H_qdot = 0, H_t = 0):
-    h_q = np.zeros(2)
-    h_qdot = np.zeros(2)
 
-    h_q[i] = H_q
-    h_qdot[i] = H_qdot
+### Define the dissipation function to be included in the Euler-Lagrange equation. j is a dummy variable needed to perform mixed partial derivatives.
+def dissipation(q, q_dot, t, j = 0):
+    """
+    Returns the dissipation function for a system with two generalized coordinates, neglecting particle-particle interactions (only diagonal terms in the dissipation matrix are non-zero).
+    """
+    q1_dot, q2_dot = q_dot
 
-    return (func(q + h_q, q_dot + h_qdot, t + H_t, j = j) - 2 * func(q, q_dot, t, j = j) + func(q - h_q, q_dot - h_qdot, t - H_t, j = j)) / (( H_q + H_qdot + H_t)**2)
+    # Zeroth order friction for mass 1 and 2
+    D_01 = (np.sign(q1_dot) if flipSignBoolean else 1) * (staticFriction[0] if np.abs(q1_dot) < 1e-5 else kineticFriction[0])*q1_dot
+    D_02 = (np.sign(q2_dot) if flipSignBoolean else 1) * (staticFriction[1] if np.abs(q2_dot) < 1e-5 else kineticFriction[1])*q2_dot
+
+    # First order friction for mass 1 and 2
+    D_11 = 1/2*linearFrictionCoefficient[0]*q1_dot**2
+    D_12 = 1/2*linearFrictionCoefficient[1]*q2_dot**2
+
+    return D_01 + D_02 + D_11 + D_12
 
 
-####
+
+### Define helper functions for repeated differentiation, with j passed as the i-variable to firstOrderDerivative
 def dL_dq(q, q_dot, t, j = 0):
-    test = firstOrderDerivative(lagrangian, q, q_dot, t, i = j, H_q = h)
-    return test
+    return firstOrderDerivative(lagrangian, q, q_dot, t, i = j, H_q = h)
 
 def dL_dqdot(q, q_dot, t, j = 0):
-    test = firstOrderDerivative(lagrangian, q, q_dot, t, i = j, H_qdot = h)
-    return test
+    return firstOrderDerivative(lagrangian, q, q_dot, t, i = j, H_qdot = h)
 
-###
-
-
+### Define the ODE to be solved. 
 def ODE(t, y):
-
+    """
+    Returns the ODE to be solved to extract the EOM for a system with two generalized coordinates.
+    """
     q1, q2, q1_dot, q2_dot = y
 
     q = np.array([q1, q2])
     q_dot = np.array([q1_dot, q2_dot])
 
-    K1 = dL_dq(q, q_dot, t, j = 0) - firstOrderDerivative(dL_dqdot, q, q_dot, t, j = 0, H_t = h)
-    K2 = dL_dq(q, q_dot, t, j = 1) - firstOrderDerivative(dL_dqdot, q, q_dot, t, j = 1, H_t = h)
+    # Euler-Lagrange equation for a system with two generalized coordinates. i and j indices are used to perform the mixed partial derivatives.
+    
+    # With each iteration, the derivatives of the Lagrangian are evaluated at the current values of q, q_dot and t.
+    K1 = dL_dq(q, q_dot, t, j = 0) - firstOrderDerivative(dL_dqdot, q, q_dot, t, j = 0, H_t = h) - firstOrderDerivative(dissipation, q, q_dot, t, i = 0, H_qdot = h)
+    K2 = dL_dq(q, q_dot, t, j = 1) - firstOrderDerivative(dL_dqdot, q, q_dot, t, j = 1, H_t = h) - firstOrderDerivative(dissipation, q, q_dot, t, i = 1, H_qdot = h)
 
     A1 = firstOrderDerivative(dL_dqdot, q, q_dot, t, i = 0, j = 0, H_q = h)
     B1 = firstOrderDerivative(dL_dqdot, q, q_dot, t, i = 0, j = 0, H_qdot = h)
@@ -231,58 +152,93 @@ def ODE(t, y):
     B2 = firstOrderDerivative(dL_dqdot, q, q_dot, t, i = 0, j = 1, H_qdot = h)
     C2 = firstOrderDerivative(dL_dqdot, q, q_dot, t, i = 1, j = 1, H_q = h)
     D2 = firstOrderDerivative(dL_dqdot, q, q_dot, t, i = 1, j = 1, H_qdot = h)
-
+    
+    # The ODE is then solved for q_dot_dot:
     q1_dot_dot = ((A2*q1_dot + C2*q2_dot - K2)*D1 - D2*(A1*q1_dot + C1*q2_dot - K1))/(B1*D2 - B2*D1)
     q2_dot_dot = ((-A2*q1_dot - C2*q2_dot + K2)*B1 + B2*(A1*q1_dot + C1*q2_dot - K1))/(B1*D2 - B2*D1)
 
-    # q1_dot_dot = (K1 - q1_dot*A1 - q2_dot*C1 - ((K2 - q1_dot*A2-q2_dot*C2)/(D2))*D1)/(B1 - (D1*B2/D2))
-    # q2_dot_dot = (K2 - q1_dot*A2 - q2_dot*C2 - ((K1 - q1_dot*A1-q2_dot*C1)/(B1))*B2)/(D2 - (B2*D1/B1))
-    
-
     return np.array([q1_dot, q2_dot, q1_dot_dot, q2_dot_dot])
-
-# Initial conditions
-q0 = np.array([1,1])
-v0 = np.array([3,1])
-y0 = np.concatenate((q0, v0))
-
-from scipy.integrate import solve_ivp
-
-tf = 20
-t = np.linspace(0, tf, 10000)
 #use solve_ivp
-sol = solve_ivp(ODE, [0, tf], y0, t_eval=t, method='RK45', rtol = 1e-6, atol = 1e-6)
-q1t = sol.y[0]
-q2t = sol.y[1]
-q1_dott = sol.y[2]
-q2_dott = sol.y[3]
-t = sol.t
+sol = solve_ivp(ODE, [0, tf], y0, t_eval=t, method='RK45', rtol=rtol, atol=atol)
+q1 = sol.y[0]
+q2 = sol.y[1]
+q1_dot = sol.y[2]
+q2_dot = sol.y[3]
 
-
-
-# #solve with odeINT
-# t = np.linspace(0, 10, 1000)
-# sol = odeint(ODE, y0, t, rtol=1e-12, atol=1e-12)
-
-# q1t = sol[:, 0]
-# q2t = sol[:, 1]
-# q1_dott = sol[:, 2]
-# q2_dott = sol[:, 3]
-
-#plot the sol
-plt.figure()
-plt.plot(t, q1t, label='Angle 1')
-plt.plot(t, q2t, label='Angle 2')
-plt.legend()
-
-
-#%%
-#total energy
+# Calculate the energy at each time step
 totalEnergy = np.zeros_like(t)
+kineticEnergy = np.zeros_like(t)
+potentialEnergy = np.zeros_like(t)
 for i in range(len(t)):
-    totalEnergy[i] = kinetic((q1t[i], q2t[i]), (q1_dott[i], q2_dott[i]), t[i]) + potential((q1t[i], q2t[i]), (q1_dott[i], q2_dott[i]), t[i])
+    q = np.array([q1[i], q2[i]])
+    q_dot = np.array([q1_dot[i], q2_dot[i]])
 
-initEnergy = totalEnergy[0]
-dE = np.abs(totalEnergy - initEnergy)/initEnergy*100
+    kineticEnergy[i] = kinetic(q, q_dot, t[i])
+    potentialEnergy[i] = potential(q, q_dot, t[i])
+    totalEnergy[i] = kineticEnergy[i] + potentialEnergy[i]
 
-plt.plot(t, dE, label='Total Energy')
+# Calculate the relative energy difference as a function of time
+dE = (totalEnergy - totalEnergy[0])/totalEnergy[0]
+
+# Plot the results
+fig, ax = figSetup(1, 3, sharex = True)
+ax[0].plot(t, q1, label=r'$q_1(t)$')
+ax[0].plot(t, q2, label=r'$q_2(t)$')
+ax[0].set_title(r'$q(t)$')
+
+ax[1].plot(t, kineticEnergy, label=r'$T(t)$')
+ax[1].plot(t, potentialEnergy, label=r'$V(t)$')
+ax[1].plot(t, totalEnergy, label=r'$E(t)$')
+
+ax[1].set_title(r'$T(t)$, $V(t)$ and $E(t)$')
+ax[2].plot(t, dE, label=r'$\Delta E(t)$')
+ax[2].set_title(r'$\Delta E(t)$')
+
+ax[0].set_xlabel(r'$t$')
+ax[1].set_xlabel(r'$t$')
+
+ax[0].legend()
+ax[1].legend()
+
+#%% Make animation
+
+# Using ffmpeg to save as mp4. ffmpeg must be downloaded and the path below changed to the correct path.
+# For some reason, adding the path to the system path does not work for me.
+plt.rcParams['animation.ffmpeg_path'] = 'C:/ffmpeg/bin/ffmpeg.exe'
+
+
+# Set up the figure and axis
+fig, ax = plt.subplots()
+totalL = np.sum(l)
+ax.set_xlim(-totalL*1.1, totalL*1.1)
+ax.set_ylim(-totalL*1.1, totalL*1.1)
+ax.set_xlabel(r'$x$ [m]')
+ax.set_ylabel(r'$y$ [m]')
+
+# Define the strings to be displayed in the upper left corner
+massString = r'$m_1 = ' + str(m[0]) + r'$ kg, $m_2 = ' + str(m[1]) + r'$ kg'
+lengthString = r'$l_1 = ' + str(l[0]) + r'$ m, $l_2 = ' + str(l[1]) + r'$ m'
+
+# Add the strings in the upper left corner in a box
+ax.text(0.05, 0.95, massString + '\n' + lengthString, transform=ax.transAxes, fontsize=12, verticalalignment='top', bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5))
+
+# Plot the pendulum as a line
+line, = ax.plot([], [], 'o-', lw=2)
+
+# Define animation function
+def animate(i):
+    x1 = np.sin(q1[i])*l[0]
+    y1 = -np.cos(q1[i])*l[0]
+    x2 = x1 + np.sin(q2[i])*l[1]
+    y2 = y1 - np.cos(q2[i])*l[1]
+
+    line.set_data([0, x1, x2], [0, y1, y2])
+    return line,
+
+
+interval = np.ceil(tf*1000/len(t))
+# Animate with duration that matches the final time of the simulation
+ani = animation.FuncAnimation(fig, animate, frames=len(t), interval=interval, blit=True, repeat=False)
+
+fps = len(t)/tf
+ani.save('double_pendulum.mp4', writer = 'ffmpeg', fps = fps, dpi = 300)
